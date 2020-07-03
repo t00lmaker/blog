1a2e02fadef47826488ffe5c93c37bc6db7b8f5c
Luan Pontes <luanpontes2@gmail.com>
2020-07-02 22:43:41 -0300

    script deploy full

--- deploy.sh
+++ deploy.sh
@@ -18,11 +18,23 @@ if [ $? -eq 0 ]; then
   export DATABASE_URL=ecto://USER:PASS@HOST/database
   
   export MIX_ENV=prod
+  export MIX_PORT=8080
 
-  mix compile \
-    && npm run deploy --prefix ./assets \
-    && mix phx.digest
-	
+  mix compile
+
+  npm -v 
+
+  if [ $? -ne 0 ]; then 
+    echo "npm not found" 
+    npm install -g npm
+  fi
+    
+  npm run deploy --prefix ./assets
+  
+  mix phx.digest
+
+  mix phx.server
+  
   exit 0
 else
   echo " mix not found"
